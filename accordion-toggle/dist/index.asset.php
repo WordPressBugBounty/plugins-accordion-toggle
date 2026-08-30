5eecc5fd07c942a9cf4e8b777644756fb4c6ef6e
Sean Murphy <sean@iamseanmurphy.com>
2026-08-30 17:28:01 +0000

    1.3.0

--- accordion-toggle/dist/index.asset.php
+++ accordion-toggle/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9ca380688dc76a13911b');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '61d3f731fe73759319ba');
